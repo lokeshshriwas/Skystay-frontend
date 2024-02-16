6f53f1b9f852d3e1b6746eadef031eab4d10efab
lokesh shriwas <lokeshshriwas2802@gmail.com>
2024-02-17 02:33:20 +0530

    changes in context

--- src/Context/Usercontext.jsx
+++ src/Context/Usercontext.jsx
@@ -6,14 +6,14 @@ const userContext = createContext({});
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [ready, setReady] = useState(false);
+  
   useEffect(() => {
-    if (!user) {
-      axios.get("/profile").then((userData) => {
-        setUser(userData.data);
-        setReady(true);
-      });
-    }
+    const userInfo = localStorage.getItem("userInfo");
+    setUser(userInfo);
+    setReady(true);
   }, []);
+
+
   return (
     <userContext.Provider value={{ user, setUser, ready }}>
       {children}
