b8a2c64dad9ee6677323d33617add10af2193472
lokesh shriwas <lokeshshriwas2802@gmail.com>
2024-02-16 16:43:08 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import {
   Profile,
   Places,
   BookingsPage,
-  Bookingpage,
+  BookingPage,
   Notfound
 } from "./pages";
 
@@ -37,7 +37,7 @@ function App() {
             <Route path="/listings/:id" element={user ? <ListingDetail /> : <Navigate to={"/login"}/>} />
             <Route path="/account" element={user ? <Profile/>: <Navigate to={"/login"}/>} />
             <Route path="/account/booking" element={user ? <BookingsPage/>: <Navigate to={"/login"}/>} />
-            <Route path="/account/booking/:id" element={user ? <Bookingpage /> : <Navigate to={"/login"}/> } />
+            <Route path="/account/booking/:id" element={user ? <BookingPage /> : <Navigate to={"/login"}/> } />
             <Route path="/account/places" element={user ? <Places />: <Navigate to={"/login"}/>} />
             <Route path="/account/places/new" element={user ? <Formpage />: <Navigate to={"/login"}/>} />
             <Route path="/account/places/:id" element={user ? <Formpage />: <Navigate to={"/login"}/>} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ import Account from "./Account/Account";
 import Home from "./Home/Home";
 import ListingDetail from "./ListingDetail/ListingDetail";
 import BookingsPage from "./Booking/BookingsPage";
-import Bookingpage from "./Booking/BookingPage";
+import BookingPage from "./Booking/BookingPage";
 import { Places, Profile } from "./Account/AccountComps";
 import AccountNav from "./Account/AccountComps/AccountNav";
 import Notfound from "./Statics/Notfound";
 
-export {Navbar, Login, Footer, Register, Account, Home, ListingDetail, BookingsPage, Bookingpage, Profile, AccountNav, Places, Notfound };
+export {Navbar, Login, Footer, Register, Account, Home, ListingDetail, BookingsPage, BookingPage, Profile, AccountNav, Places, Notfound };
