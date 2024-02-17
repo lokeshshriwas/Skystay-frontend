bc016c5464af7b603ad0c959047a82b1b38adfd9
lokesh shriwas <lokeshshriwas2802@gmail.com>
2024-02-17 17:46:28 +0530

    refresh problem fixed

--- src/pages/Statics/Navbar.jsx
+++ src/pages/Statics/Navbar.jsx
@@ -50,16 +50,16 @@ const Navbar = () => {
 
   return (
     <header className="flex justify-between max-[380px]:gap-4 ">
-      <div onClick={reRender} className="flex items-center gap-1 max-[200px]:hidden cursor-pointer">
+      <Link to={"/"} onClick={reRender} className="flex items-center gap-1 max-[200px]:hidden cursor-pointer">
         {icons.logo}
         <span className="font-bold text-xl max-[630px]:hidden">Skystay</span>
-      </div>
+      </Link>
 
       {!searchClicked ? (
         <div className="flex border border-gray-300 rounded-full py-2 px-4 gap-2 shadow-md shadow-gray-300 ">
-          <div onClick={reRender} className="cursor-pointer" >
+          <Link to={"/"} onClick={reRender} className="cursor-pointer" >
             <div>Anywhere</div>
-          </div>
+          </Link>
           <div className="border-l border-gray-300 max-[500px]:hidden"></div>
           <Link to={"/account/booking"}>
           <div className="max-[500px]:hidden">Your Bookings</div>
